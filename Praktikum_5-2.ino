7a7d62b8b47084c3e573d68485a837c48f44d3c5
Syifa Dzikri Tsani <syifadzikri@gmail.com>
2016-10-04 15:18:04 +0700

    Add files via upload

---
+++ Praktikum_5-2.ino
@@ -0,0 +1,13 @@
+void setup() { 
+  Serial.begin(115200);
+}
+
+void loop() { 
+  Serial.print(0x00); 
+  Serial.print(0x01); 
+  Serial.print(0XF0); 
+  Serial.print(0x1F); 
+  Serial.print(0x2F); 
+  delay(2000);
+}
+
---
+++ Praktikum_5-4.ino
@@ -0,0 +1,51 @@
+#include <LiquidCrystal.h>
+#define FOSC 16000000
+LiquidCrystal lcd(2,3,4,5,6,7); 
+String dataSerial;
+
+void initSerial(int baudrate) { 
+  int ubrr = FOSC/16/baudrate-1;
+// Mengisi register UBRR0H dan UBRR0L untuk menentukan baudrate 
+UBRR0H |= ubrr>>8;
+UBRR0L |= ubrr;
+// Mengaktifkansaluran transmitter dan receiver
+UCSR0B |= (1<<RXEN0)|(1<<TXEN0);
+// Menggunakan format data 8 bit
+UCSR0C |= (0<<UCSZ02)|(1<<UCSZ01)|(1<<UCSZ00);
+// Menggunakan 1 stop bit
+UCSR0C |= (0<<USBS0);
+// No Parity
+UCSR0C |= (0<<UPM01)|(0<<UPM00);
+// Mengaktifkan RX interrupt
+UCSR0B |= (1<<RXCIE0);
+}
+
+ 
+// Rutin yang akan dijalankan jika terjadi interupsi
+ISR(USART_RX_vect){
+  char data = UDR0; 
+   if(data=='\n') {
+    lcd.clear();
+    lcd.setCursor(0,0); 
+    lcd.print("IMMPB"); 
+    lcd.setCursor(0,1); 
+    lcd.print(dataSerial);
+    dataSerial = "";
+}
+   else{
+    if(data!='\r') {
+      dataSerial += data;
+      }
+    }
+  }
+  void setup() { 
+    initSerial(9600); 
+    lcd.begin(16,2);
+    lcd.clear();
+    sei();
+  }
+  
+  void loop() {
+  
+  }
+
